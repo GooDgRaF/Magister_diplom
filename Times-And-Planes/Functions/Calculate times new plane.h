7ac31a53602d7c2e3102f5fdad39e6e04aa9ef1f
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2021-01-18 16:50:20 +0500

    Update Calculate times new plane.h

--- Times-And-Planes/Functions/Calculate times new plane.h
+++ Times-And-Planes/Functions/Calculate times new plane.h
@@ -7,6 +7,6 @@
 
 #include <Zone.h>
 
-void
-calc_plane(Zone &zone, const PlanePoint &plane, string &flow_name, string &check_point_there, std::pair<string, string> &edge_plane);
+int
+calc_plane(Zone &zone, const PlanePoint &plane, const string &flow_name, const string &check_point_there, const pair<string, string> &edge_plane_point);
 #endif //MAIN_CPP_CALCULATE_TIMES_NEW_PLANE_H
