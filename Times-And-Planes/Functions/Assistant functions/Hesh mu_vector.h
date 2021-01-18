190cd6f3b463fd72b41938e70bbdaaacd8b914f3
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2021-01-18 16:49:34 +0500

    Считает сумму символов в каждом токене из строки

---
+++ Times-And-Planes/Functions/Assistant functions/Hesh mu_vector.h
@@ -0,0 +1,13 @@
+//
+// Created by Антон on 18.01.2021.
+//
+
+#ifndef MAIN_CPP_HESH_MU_VECTOR_H
+#define MAIN_CPP_HESH_MU_VECTOR_H
+
+#include <string>
+#include <vector>
+
+void sum_char_mu(const std::string &str, std::vector<int> &v);
+
+#endif //MAIN_CPP_HESH_MU_VECTOR_H
