d629606cc6ad042bd8fc7991fc33c8a578c71d2a
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2021-01-20 18:53:10 +0500

    Update main.cpp

--- Times-And-Planes/main.cpp
+++ Times-And-Planes/main.cpp
@@ -14,43 +14,39 @@ int main()
     {
         Zone zone;
 
-        string nameOfPointsFile = "../Source information/Points/KoltsovoStScheme.txt";//Test.txt
-        string nameOfSchemeFile = "../Source information/Schemes/KoltsovoStScheme.txt";//Test.txt
-        string nameOfFlowsFile = "../Source information/Flows/Koltsovo.txt";//Test
+        string nameOfPointsFile = "../Source information/Points/Test.txt";//KoltsovoStScheme
+        string nameOfSchemeFile = "../Source information/Schemes/Test.txt";//KoltsovoStScheme
+        string nameOfFlowsFile = "../Source information/Flows/Test.txt";//Koltsovo
 
         stage_minus_one(nameOfPointsFile, nameOfSchemeFile, nameOfFlowsFile, zone);
 
-        PlanePoint plane1;
-        plane1.x = Coordinate::createKms(-180);
-        plane1.y = Coordinate::createKms(100);
-        plane1.z = Coordinate::createMs(5700);
-        plane1.V = Velocity::createVm_s(138);
-        plane1.flow_for_plane = "RALUB";
-        plane1.destination = "TUNED";
-        plane1.on_edge = {"RALUB", "TUNED"};
+//        PlanePoint plane1;
+//        plane1.x = Coordinate::createKms(-180);
+//        plane1.y = Coordinate::createKms(100);
+//        plane1.z = Coordinate::createMs(5700);
+//        plane1.V = Velocity::createVm_s(138);
+//        plane1.flow_for_plane = "RALUB";
+//        plane1.destination = "TUNED";
+//        plane1.on_edge = {"RALUB", "TUNED"};
 //
-//        PlanePoint plane2;
-//        plane2.x = Coordinate::createKms(-0.5);
-//        plane2.y = Coordinate::createKms(1);
-//        plane2.z = Coordinate::createKms(0);
-//        plane2.V = Velocity::createVkm_h(13);
-//        plane2.flow_for_plane = "Flow 1";
-//        plane2.destination = "A2";
-//        plane2.on_edge = {"A1", "A2"};
+        PlanePoint plane2;
+        plane2.x = Coordinate::createKms(-1);
+        plane2.y = Coordinate::createKms(0.5);
+        plane2.z = Coordinate::createKms(0);
+        plane2.V = Velocity::createVkm_h(10);
+        plane2.flow_for_plane = "Flow 1";
+        plane2.destination = "A1";
+        plane2.on_edge = {"A0", "A1"};
 
 
-        calc_plane(zone, plane1);
+        calc_plane(zone, plane2);
+
+
+
+
 //        calc_plane(zone, plane2);
-        calculateTimes(zone, zone.flows[0]);
-        zone.flows[0].print_times();
-
-
-
-
-        //time_segment()
-
-
-
+       calculateTimes(zone, zone.flows[0]);
+       zone.flows[0].print_times();
 
 
 
