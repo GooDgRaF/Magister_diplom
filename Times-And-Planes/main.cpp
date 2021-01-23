a780658dfb1649d64f13a4a9c75c4cea9703b6fa
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2021-01-23 12:49:16 +0500

    Update main.cpp

--- Times-And-Planes/main.cpp
+++ Times-And-Planes/main.cpp
@@ -30,13 +30,13 @@ int main()
 //        plane1.on_edge = {"RALUB", "TUNED"};
 //
         PlanePoint plane2;
-        plane2.x = Distance::createKms(0.25);
+        plane2.x = Distance::createKms(-1.25);
         plane2.y = Distance::createKms(1.25);
         plane2.z = Distance::createKms(0);
         plane2.V = Velocity::createVkm_h(10);
         plane2.flow_for_plane = "Flow 1";
-        plane2.destination = "St2";
-        plane2.on_edge = {"A2", "St2"};
+        plane2.destination = "A2";
+        plane2.on_edge = {"A1", "A2"};
 
 
 
